d0b43ab5ac62479a1f85a04de7e4f46642f3aeae
Mahsa <m.hamedd1380@gmail.com>
2024-08-19 19:33:29 +0330

    Initial commit

---
+++ Controllers/LogInController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Library.Models;
+using System.Diagnostics;
+
+namespace Library.Controllers
+{
+    public class LogInController : Controller
+    {
+
+        private readonly ILogger<LogInController> _logger;
+        private static List<Person> personData = new List<Person>() {
+            new Person { Username="mhsa", Password="hamed" },
+        };
+
+        public LogInController(ILogger<LogInController> logger)
+        {
+            _logger = logger;
+
+        }
+
+        public IActionResult LogIn()
+        {
+            return View(personData);
+        }
+
+        public IActionResult Form() { 
+            return View(); 
+        }
+       
+        
+        [HttpPost]
+        public IActionResult Form([FromForm] Person person)
+        {
+            
+                // Check if the form data exists in the list
+                var match = personData.FirstOrDefault(x => x.Username == person.Username && x.Password == person.Password);
+
+                if (match != null)
+                {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+                {
+                    ViewBag.Message = "No match found.";
+                return View(person);
+            }
+
+
+            
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.Models
+{
+    public class Person
+    {
+        public int Id { get; set; } 
+        public string Firstname { get; set; }    
+        public string Lastname { get; set; }
+        public string Username { get; set; }   
+        public string Password { get; set; }
+    }
+}
