e8d498b93e994c7489ec1f578f9d4242096dc66b
Mahsa <m.hamedd1380@gmail.com>
2024-08-26 01:23:51 +0330

    Sign up

--- Controllers/LogInController.cs
+++ Controllers/LogInController.cs
@@ -1,16 +1,17 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 using Library.Models;
 using System.Diagnostics;
+using System.Reflection;
 
 namespace Library.Controllers
 {
     public class LogInController : Controller
     {
-
         private readonly ILogger<LogInController> _logger;
         private static List<Person> personData = new List<Person>() {
-            new Person { Username="mhsa", Password="hamed" },
+            new Person { Username="mhsa", Password="hamed12345" },
         };
 
         public LogInController(ILogger<LogInController> logger)
@@ -24,35 +25,72 @@ namespace Library.Controllers
             return View(personData);
         }
 
-        public IActionResult Form() { 
-            return View(); 
-        }
-       
-        
-        [HttpPost]
-        public IActionResult Form([FromForm] Person person)
-        {
-            
-                // Check if the form data exists in the list
-                var match = personData.FirstOrDefault(x => x.Username == person.Username && x.Password == person.Password);
-
-                if (match != null)
-                {
-                return RedirectToAction("Index", "Home");
-            }
-            else
-                {
-                    ViewBag.Message = "No match found.";
-                return View(person);
-            }
-
-
-            
-        }
-        public IActionResult Index()
+        public IActionResult Form()
         {
             return View();
         }
+
+
+        [HttpPost]
+        public IActionResult Form([FromForm] Person person)
+        {
+
+            // Check if the form data exists in the list
+            var match = personData.FirstOrDefault(x => x.Username == person.Username && x.Password == person.Password);
+
+            if (match != null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Message = "No match found.";
+                return View(person);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult SignUp([FromForm] Person person)
+        {
+            // Check if the username already exists in the list
+            var existingUser = personData.FirstOrDefault(u => u.Username == person.Username);
+
+            if (existingUser != null)
+            {
+                // Username already exists, return an error message or view
+                ViewBag.Message = "Username already exists. Please choose a different one.";
+                return View(person); // Return the same view with the error message
+            }
+
+            // Create a new user
+            var user = new Person
+            {
+                Username = person.Username,
+                Password = person.Password,
+            };
+
+            // Add the user to the in-memory list
+            personData.Add(user);
+
+            // Redirect to a confirmation page or login page
+            return RedirectToAction("Users", "LogIn");
+        }
+
+        // Optionally, a method to display all users (for testing purposes)
+        [HttpGet]
+        public IActionResult Users()
+        {
+            return View(personData);
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- Models/Person.cs
+++ Models/Person.cs
@@ -4,10 +4,18 @@ namespace Library.Models
 {
     public class Person
     {
-        public int Id { get; set; } 
-        public string Firstname { get; set; }    
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Firstname { get; set; }
+        [Required]
         public string Lastname { get; set; }
-        public string Username { get; set; }   
+        [Required(ErrorMessage = "Username is required.")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Username must be between 3 and 100 characters.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters.")]
         public string Password { get; set; }
     }
 }
